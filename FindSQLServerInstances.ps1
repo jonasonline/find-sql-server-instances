ea6fbbfbbd65565c601fa2e54bf7b3865a065f0b
Jonas Carlsson <jonas.carlsson@atea.se>
2017-02-01 12:58:38 +0100

    Changing from ini file to json

--- FindSQLServerInstances.ps1
+++ FindSQLServerInstances.ps1
@@ -1,13 +1,9 @@
 ﻿Import-Module ServerManager
 
-if ((Test-Path config.ini) -eq $false) {
-    Get-Content -Path "config.template.ini" | Out-File -FilePath "config.ini"
-    Write-Warning "Created new configuration file from template"
-    exit
-} 
-Get-Content "config.ini" | foreach-object -begin {$ScriptConfiguration=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $ScriptConfiguration.Add($k[0], $k[1..($k.Count - 1)].Replace("`"", "") -join "=") } }
-$ErrorLogPath = $ScriptConfiguration.ErrorLogPath
-$OutputPath = $ScriptConfiguration.OutputPath
+if ((Test-Path -Path "config.json") -eq $false -or $Init -eq $true) {
+    Get-Content -Path ".\config.jsontemplate.json" | Out-File -FilePath "config.json"
+}
+$ScriptConfiguration = (Get-Content "config.json") -Join "`n" | ConvertFrom-Json
 
 $ADPowershellModule = Get-WindowsFeature RSAT-AD-PowerShell
 if ($ADPowershellModule.Installed -eq $false) {
@@ -27,8 +23,7 @@ if ($DomainName -eq $null) {
     exit
 }
 
-$OUPaths = $OUPaths.Split(";")
-New-Item -ItemType file -Force -Path $OutputPath | Out-Null
-ForEach ($OUPath in $OUPaths) {
+New-Item -ItemType file -Force -Path $ScriptConfiguration.OutputPath | Out-Null
+ForEach ($OUPath in $ScriptConfiguration.OUPaths) {
     Get-ADComputer -SearchBase $OUPath -Filter {(enabled -eq "true") -and -(OperatingSystem -Like "Windows *")} -SearchScope OneLevel -ResultPageSize 10000 -Server $DomainName | ForEach-Object{try{$currentDate = Get-Date; $recentError = $null; Get-Service -ComputerName $_.Name -ErrorVariable recentError -ErrorAction SilentlyContinue | Where-Object {$_.name -eq "MSSQLSERVER" -or $_.name -like "MSSQL$*" -or $_.displayName -like "SQL Server (*"};}catch{Write-Warning $_;Out-File -FilePath $ErrorLogPath -Append -InputObject $currentDate; Out-File -FilePath $ErrorLogPath -Append -InputObject $_};}| Select-Object MachineName, DisplayName, Status | Export-Csv -NoClobber -NoTypeInformation -Path $OutputPath -Append
 }
