c121a327018bc97dd2b4604d7706992a921c7997
Jonas Carlsson <jonas.carlsson@atea.se>
2017-02-01 12:09:42 +0100

    Fixed error where server where not found

--- FindSQLServerInstances.ps1
+++ FindSQLServerInstances.ps1
@@ -30,7 +30,5 @@ if ($DomainName -eq $null) {
 $OUPaths = $OUPaths.Split(";")
 New-Item -ItemType file -Force -Path $OutputPath | Out-Null
 ForEach ($OUPath in $OUPaths) {
-    Get-ADComputer -SearchBase $OUPath -Filter {(enabled -eq "true")} -SearchScope OneLevel -ResultPageSize 10000 -Server $DomainName | ForEach{try{$currentDate = Get-Date; $recentError = $null; Get-Service -ComputerName $_.Name -Name "MSSQLSERVER", "MSSQL$*" -ErrorVariable recentError -ErrorAction SilentlyContinue;}catch{Write-Warning $_;Out-File -FilePath $ErrorLogPath -Append -InputObject $currentDate; Out-File -FilePath $ErrorLogPath -Append -InputObject $_};}| select MachineName, DisplayName, Status | Export-Csv -NoClobber -NoTypeInformation -Path $OutputPath -Append
+    Get-ADComputer -SearchBase $OUPath -Filter {(enabled -eq "true") -and -(OperatingSystem -Like "Windows *")} -SearchScope OneLevel -ResultPageSize 10000 -Server $DomainName | ForEach-Object{try{$currentDate = Get-Date; $recentError = $null; Get-Service -ComputerName $_.Name -ErrorVariable recentError -ErrorAction SilentlyContinue | Where-Object {$_.name -eq "MSSQLSERVER" -or $_.name -like "MSSQL$*" -or $_.displayName -like "SQL Server (*"};}catch{Write-Warning $_;Out-File -FilePath $ErrorLogPath -Append -InputObject $currentDate; Out-File -FilePath $ErrorLogPath -Append -InputObject $_};}| Select-Object MachineName, DisplayName, Status | Export-Csv -NoClobber -NoTypeInformation -Path $OutputPath -Append
 }
-
-$JonasTestar = "Test"
